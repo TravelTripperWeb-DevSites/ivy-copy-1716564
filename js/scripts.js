6702657985194076861a9279a12b5c6b43c580a1
dmanikym <dmanikyam@traveltripper.com>
2016-09-23 15:18:06 +0530

    attractions updates

--- js/scripts.js
+++ js/scripts.js
@@ -66,7 +66,26 @@ $(".campaign-item").height(maxHeight);
           // itemsMobile : false
      
       });
-     
+	  
+	  $(".filter").on("click", function () {
+    var $this = $(this);
+    // if we click the active tab, do nothing
+    if (!$this.hasClass("active")) {
+      $(".filter").removeClass("active");
+      $this.addClass("active"); // set the active tab
+      var $filter = $this.data("rel"); // get the data-rel value from selected tab and set as filter
+      $filter == 'all' ? // if we select "view all", return to initial settings and show all
+        $(".fancybox").attr("data-fancybox-group", "gallery").not(":visible").fadeIn() 
+        : // otherwise
+        $(".fancybox").fadeOut(0).filter(function () { 
+          return $(this).data("filter") == $filter; // set data-filter value as the data-rel value of selected tab
+        }).attr("data-fancybox-group", $filter).fadeIn(1000); // set data-fancybox-group and show filtered elements
+    } // if
+  }); // on
+	 
+     $('#custom-easing').lightGallery({
+		cssEasing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+	});
     });
 
 $(window).on('load scroll resize', function () {
